010394cc54209dee633a34e1e5c9c7cd4c4c3d28
aamir <wasi3aamir@gmail.com>
2019-02-01 21:32:50 +0600

    Not Accepted

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            for (int t = 1; t <= T; t++)
+            {
+                ushort Snum = ushort.Parse(Console.ReadLine());
+                if (PerfectSquare(Snum))
+                {
+                    Console.WriteLine($"Case {t}: YES", t);
+                }
+                else
+                {
+                    Console.WriteLine($"Case {t}: NO", t);
+                }
+            }
+        }
+        public static bool PerfectSquare(ushort n)
+        {
+            ushort root = (ushort)Math.Sqrt(n);
+            return (int)Math.Pow(root, 2) == n;
+        }
+    }
+}
